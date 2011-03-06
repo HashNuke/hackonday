3e54f27f5105934d5c20d8e7fa323c369afa980d
Akash Manohar J <akash@akash.im>
2011-03-06 17:00:14 +0530

    init commit
    Signed-off-by: Akash Manohar J <akash@akash.im>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CoHack::Application.config.secret_token = '182a063b1f93ce12d19adf5a1b6eda9ecf420a716d898960be2b680d8526781daa2090946bcee4f27697bc33f89fa74437ac6f831323bddb8aa80cedb4259e6b'
