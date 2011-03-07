dbea0bb4a9f063a2eca0dd2313db0a5c0654b53b
Akash Manohar J <akash@akash.im>
2011-03-07 21:24:50 +0530

    added hacker as default role in the migrations
    Signed-off-by: Akash Manohar J <akash@akash.im>

--- db/schema.rb
+++ db/schema.rb
@@ -34,9 +34,9 @@ ActiveRecord::Schema.define(:version => 20110307151223) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "email",                               :default => "", :null => false
-    t.string   "encrypted_password",   :limit => 128, :default => "", :null => false
-    t.string   "password_salt",                       :default => "", :null => false
+    t.string   "email",                               :default => "",       :null => false
+    t.string   "encrypted_password",   :limit => 128, :default => "",       :null => false
+    t.string   "password_salt",                       :default => "",       :null => false
     t.string   "reset_password_token"
     t.string   "remember_token"
     t.datetime "remember_created_at"
@@ -47,7 +47,7 @@ ActiveRecord::Schema.define(:version => 20110307151223) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "role"
+    t.string   "role",                                :default => "hacker"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
