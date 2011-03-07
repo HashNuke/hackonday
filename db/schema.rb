dd668171c5f9a4bb19756576a66777213c6d0d92
Akash Manohar J <akash@akash.im>
2011-03-07 20:42:42 +0530

    added role field to users
    Signed-off-by: Akash Manohar J <akash@akash.im>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110306131456) do
+ActiveRecord::Schema.define(:version => 20110307151223) do
 
   create_table "hackthaons", :force => true do |t|
     t.string   "name"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(:version => 20110306131456) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "role"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
