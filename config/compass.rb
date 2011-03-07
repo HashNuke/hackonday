5fd915b0eca9146c61d6fcc3340791cd9cb06227
Akash Manohar J <akash@akash.im>
2011-03-08 01:40:16 +0530

    fixed sass heroku error
    Signed-off-by: Akash Manohar J <akash@akash.im>

--- config/compass.rb
+++ config/compass.rb
@@ -12,3 +12,5 @@ sass_dir = "app/stylesheets"
 environment = Compass::AppIntegration::Rails.env
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
+
+Compass.configuration.sass_options = { :never_update => true } if Rails.env.production? 
