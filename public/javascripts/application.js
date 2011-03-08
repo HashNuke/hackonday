b4ee22883d2c574047532867d0475f12467656dd
Akash Manohar J <akash@akash.im>
2011-03-08 23:25:13 +0530

    moved code from application.js to landing.js
    Signed-off-by: Akash Manohar J <akash@akash.im>

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,27 +1,3 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 //
-
-
-// http://tenseveninteractive.com/content/jquery-countdown-background-and-overlay
-$(function () {
-  $('#launchcountdown').countdown({
-    until: $.countdown.UTCDate(-8, 2011, 3 - 1, 19, 9),
-    format: 'DHMS',
-    layout: '<div id="t7_timer">'+
-    '<div id="t7_vals">'+
-    '<div id="t7_d" class="t7_numbs">{dnnn}</div>'+
-    '<div id="t7_h" class="t7_numbs">{hnn}</div>'+
-    '<div id="t7_m" class="t7_numbs">{mnn}</div>'+
-    '<div id="t7_s" class="t7_numbs">{snn}</div>'+
-    '</div>'+
-    '<div id="t7_labels">'+
-    '<div id="t7_dl" class="t7_labs">days</div>'+
-    '<div id="t7_hl" class="t7_labs">hours</div>'+
-    '<div id="t7_ml" class="t7_labs">mins</div>'+
-    '<div id="t7_sl" class="t7_labs">secs</div>'+
-    '</div>'+
-    '<div id="t7_timer_over"></div>'+
-    '</div>'
-  });
-});       
--- public/javascripts/landing.js
+++ public/javascripts/landing.js
@@ -7,4 +7,26 @@ $(document).ready(function(){
     $("#notification_form").bind("ajax:success", function(e, data, status, xhr){
         console.log("success");
     });
+
+// http://tenseveninteractive.com/content/jquery-countdown-background-and-overlay
+    $('#launchcountdown').countdown({
+        until: $.countdown.UTCDate(-8, 2011, 3 - 1, 19, 9),
+        format: 'DHMS',
+        layout: '<div id="t7_timer">'+
+            '<div id="t7_vals">'+
+            '<div id="t7_d" class="t7_numbs">{dnnn}</div>'+
+            '<div id="t7_h" class="t7_numbs">{hnn}</div>'+
+            '<div id="t7_m" class="t7_numbs">{mnn}</div>'+
+            '<div id="t7_s" class="t7_numbs">{snn}</div>'+
+            '</div>'+
+            '<div id="t7_labels">'+
+            '<div id="t7_dl" class="t7_labs">days</div>'+
+            '<div id="t7_hl" class="t7_labs">hours</div>'+
+            '<div id="t7_ml" class="t7_labs">mins</div>'+
+            '<div id="t7_sl" class="t7_labs">secs</div>'+
+            '</div>'+
+            '<div id="t7_timer_over"></div>'+
+            '</div>'
+    });
+
 });
