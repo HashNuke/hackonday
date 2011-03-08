cba3628311e3a443e501e48bd44eb1a49a10536e
jessor <daniel@enthusiasm.cc>
2011-03-08 18:56:08 +0100

    added notifications to landing site

--- public/javascripts/landing.js
+++ public/javascripts/landing.js
@@ -1,10 +1,19 @@
 $(document).ready(function(){
 
     $("#notification_form").bind("ajax:error", function(e, xhr, status, error){
-        console.log(xhr.responseText);
+        if(xhr.responseText != 'undefined') {
+          console.log(xhr.responseText);
+          var response = jQuery.parseJSON(xhr.responseText);
+          $("#formreply").html("Sorry, " + response.email + ".");
+        } else {
+          $("#formreply").html("Sorry, some error occured. Please try again.");
+        }
+        $("#formreply").removeClass("success").addClass("error");
     });
 
     $("#notification_form").bind("ajax:success", function(e, data, status, xhr){
         console.log("success");
+        $("#formreply").html("Thanks! You'll hear from us soon.");
+        $("#formreply").removeClass("error").addClass("success");
     });
 });
