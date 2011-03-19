77ab9f4b09380709fd7500e6998316b228e6dae9
Akash Manohar J <akash@akash.im>
2011-03-19 17:22:45 +0530

    added view for main/home
    Signed-off-by: Akash Manohar J <akash@akash.im>

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,8 @@
 class MainController < ApplicationController
+  
+  before_filter :current_user, :only =>[:home]
+  load_and_authorize_resource
+  
   def landing
     @invite_request = InviteRequest.new
     render :layout=>'landing'
@@ -6,6 +10,10 @@ class MainController < ApplicationController
 
   def uc
     render :text=>InviteRequest.count
+  end
+
+  def home
+    
   end
   
 end
